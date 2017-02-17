6098f4a7ad3287fcf17a477d3d94103205ec51eb
nashvent <anibal.ventura@ucsp.edu.pe>
2017-02-17 01:01:01 -0500

    mas avances con cotizar

--- RESPALDO/formulario.php
+++ RESPALDO/formulario.php
@@ -1,5 +1,5 @@
 <?php 
-	$servicio=array(array("Selecciona dato","0"),array("servicio1","20"),array("servicio2","32"),array("servicio3","12"),array("servicio4",19));
+	$servicio=array(array("servicio1","20"),array("servicio2","32"),array("servicio3","12"),array("servicio4",19));
 	$tam=count($servicio);
 	$opcion="";
 	for ($i=0; $i<$tam ; $i++) { 
@@ -36,14 +36,31 @@
 		<form id="form" name="form" method="post" action="">  
 			<input type="text" value="1" id="total" name="total" style="visibility: hidden;">
 			<div id="div_1" class="row">
-		    	<select class="col-md-2 importe_linea" value="" id="servicio1" name="servicio1" onchange="refresh(this)">
+		    	<select class="col-md-2" id="servicio1" name="servicio1" onchange="myFunction()">
 					  <?php echo $opcion; ?>
 		    	</select> 
-		     	<p class="col-md-2" id="demo1" name="demo"></p>
+		     	<input class="col-md-2" id="costo1" type="text" name="costo1" value=""/>
 		     	<input class="col-md-2 bt_plus" id="1" type="button" value="+" />
 			</div>
-			Total: <input type="text" id="total_final" value="0"/>
+
+			<button type="submit" class="boton">Save</button>
 		</form>
 	</div>
+	
+<select id="mySelect" onchange="myFunction()">
+  <option value="Audi">Audi
+  <option value="BMW">BMW
+  <option value="Mercedes">Mercedes
+  <option value="Volvo">Volvo
+</select>
+	<p id="demo"></p>
+
+
+<script>
+function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    document.getElementById("demo").innerHTML = "You selected: " + x;
+}
+</script>
 </body>
 </html>
--- js/jquery.addfield.js
+++ js/jquery.addfield.js
@@ -1,6 +1,29 @@
+function calcular_total() {
+	var importe_total = 0;
+	$(".importe_linea").each(
+		function(index, value) {
+			if (eval($(this).val())) {
+			importe_total = importe_total + parseInt(servicio[eval($(this).val())][1]);
+			}
+		}
+	);
+	console.log(importe_total);
+	document.getElementById("total_final").value=importe_total;
+	return importe_total;
+}
+
+function refresh(objeto) {
+	var id=objeto.id.substring(8);
+	var op=document.getElementById("servicio"+id).value;
+    document.getElementById("demo"+id).innerHTML = "s/. " + servicio[parseInt(op)][1];
+    calcular_total();
+}
+
+
 $(document).ready(function() {
-		$(".bt_plus").each(function (el){	
-			$(this).bind("click",addField);
+	document.getElementById("demo1").innerHTML = "s/. " + calcular_total().toString();
+	$(".bt_plus").each(function (el){	
+		$(this).bind("click",addField);
 	});
 });
 
@@ -10,38 +33,19 @@ var clickID = parseInt($(this).parent('div').attr('id').replace('div_',''));
 var newID = (clickID+1);
 $("#total").val(newID);
 
-// Creo un clon del elemento div que contiene los campos de texto
 $newClone = $('#div_'+clickID).clone(true);
-
-//Le asigno el nuevo numero id
 $newClone.attr("id",'div_'+newID);
-
-//Asigno nuevo id al primer campo input dentro del div y le borro cualquier valor que tenga asi no copia lo ultimo que hayas escrito.(igual que antes no es necesario tener un id)
-$newClone.children("select").eq(0).attr("id",'servicio'+newID).attr("name",'servicio'+newID).val('');
-
-//Borro el valor del segundo campo input(este caso es el campo de cantidad)
-$newClone.children("input").eq(1).attr("id",'costo'+newID).attr("name",'costo'+newID).val('');
-
-//Asigno nuevo id al boton
-$newClone.children("input").eq(2).attr("id",newID)
-
-//Inserto el div clonado y modificado despues del div original
+$newClone.children("select").eq(0).attr("id",'servicio'+newID).attr("name",'servicio'+newID).val('0');
+$newClone.children("p").eq(0).attr("id",'demo'+newID).html("");
+$newClone.children("input").eq(0).attr("id",newID);
 $newClone.insertAfter($('#div_'+clickID));
-
-//Cambio el signo "+" por el signo "-" y le quito el evento addfield
 $("#"+clickID).val('-').unbind("click",addField);
-
-
-//Ahora le asigno el evento delRow para que borre la fial en caso de hacer click
-$("#"+clickID).bind("click",delRow);					
-
-			   
+$("#"+clickID).bind("click",delRow);							   
 }
 
 
 function delRow() {
-// Funcion que destruye el elemento actual una vez echo el click
-$(this).parent('div').remove();
-$("#total").val(parseInt($("#total").val())-1);
-
+	$(this).parent('div').remove();
+	$("#total").val(parseInt($("#total").val())-1);
+	calcular_total();
 }
